31026cdd53d68a1a1ba3833ff6320ed10c4164e5
faizah1992 <faizah1992@gmail.com>
2020-03-24 12:01:34 -0500

    base project

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_24_162429) do
+ActiveRecord::Schema.define(version: 2020_03_24_165912) do
 
   create_table "cupcakes", force: :cascade do |t|
     t.string "name"
+  end
+
+  create_table "customers", force: :cascade do |t|
+    t.string "name"
+  end
+
+  create_table "orders", force: :cascade do |t|
+    t.integer "customer_id"
   end
 
 end
