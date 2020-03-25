d7e162f67ed32addf40da56f3c6f9a2c356fdbaa
Linxerz <pruittj2000@Yahoo.com>
2020-03-25 11:52:34 -0500

    Cleaned it up

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -2,4 +2,18 @@ class Customer < ActiveRecord::Base
     has_many :orders
     has_many :cupcakes, through: :orders 
 
+    def self.customer_info
+        ### Check if information already in the database
+        puts "What is the name of your order?"
+            name = gets.chomp
+        puts "What is your Address?"
+            address = gets.chomp
+        puts "What is your phone number?"
+            phone_number = gets.chomp
+        puts "What is your email address?"
+            email_address = gets.chomp
+        
+        Customer.find_or_create_by(name: name, address: address, phone_number: phone_number, email_address: email_address)
+    end
+
 end
--- project.rb
+++ project.rb
@@ -1,76 +1,55 @@
 require_relative './config/environment.rb'
 
 
+def start_app
+    puts "Hi, Welcome to Bake My Day!"
 
-puts "Hi, Welcome to Bake My Day!"
+    prompt = TTY::Prompt.new()
 
-prompt = TTY::Prompt.new()
-
-user_choice = prompt.select("What would you like to do today?", [
-    "Place an Order",
-    "Browse Cupcakes",
-    "Exit"
-])
-
-Cupcake.destroy_all
-
-Cupcake.create(name: "Oh My Godiva", flavor: "Chocolate", price: 8, calories: 550)
-Cupcake.create(name: "Ooey Gooey", flavor: "PBnJ", price: 3.50, calories: 420)
-Cupcake.create(name: "I can't believe it's not a cupcake", flavor: "Nothing", price: 0, calories: 0)
-Cupcake.create(name: "Strawberry Shortcake", flavor: "Strawberry Cheesecake", price: 6.50, calories: 400)
-Cupcake.create(name: "How Orignal...", flavor: "Vanilla", price: 4.00, calories: 360)
-Cupcake.create(name: "Best of Both Worlds", flavor: "Marble", price: 5.50, calories: 450)
-
-### Put into a proper print function or whatever
+    user_choice = prompt.select("What would you like to do today?", [
+        "Place an Order",
+        "Browse Cupcakes",
+        "Exit"
+    ])
 
 
-if user_choice == "Place an Order"
-    Cupcake.print
-    user_choice = prompt.select("Choose 1 cupcake", 
-        Cupcake.all.map{|cupcake| cupcake.name}
-    )
-    Cupcake.all.select{|cupcake| cupcake.name == user_choice}
+    if user_choice == "Place an Order"
+        ###Limit in case more cupcakes get created
+        Cupcake.print
+        user_choice = prompt.select("Choose a cupcake", 
+            Cupcake.all.map{|cupcake| cupcake.name}
+        )
 
-    puts "How many cupcakes would you like?"
-        quanity = gets.chomp
- 
-elsif user_choice == "Browse Cupcakes"
-    Cupcake.print
+        a = Cupcake.all.select{|cupcake| cupcake.name == user_choice}
+        binding.pry
+        customer = Customer.customer_info
+
+
+        # puts "How many cupcakes would you like?"
+        #     quanity = gets.chomp
+    
+    elsif user_choice == "Browse Cupcakes"
+        Cupcake.print
+    end
+
+
+    ### Turn into a method later
+    ### Learn how to empty out a database
+    ### Check
+
+
+
+
+
+
+
+
+
+    #Based on whether Y/N
+    #If yes then we would ask them their info for the order ~~~ Is when we initialize
+    #
+    #
+    #If no we can let them browse the cupcakes for sale (lol window shopping)
 end
 
-
-### Turn into a method later
-### Learn how to empty out a database
-### Check
-
-
-
-
-         #Please no deleto
-#-----------------------------------------#
-# if user_choice == "Place an Order"
-#     puts "What is the name of your order?"
-#         name = gets.chomp
-#     puts "What is your Address?"
-#         address = gets.chomp
-#     puts "What is your phone number?"
-#         phone_number = gets.chomp
-#     puts "What is your email address?"
-#         email_address = gets.chomp
-    
-#     Customer.create(name: name,address: address,phone_number: phone_number,email_address: email_address)
-#     Customer.delete(name = "")
-#     binding.pry
-# else
-#     nil
-# end
-
-
-
-
-
-#Based on whether Y/N
-#If yes then we would ask them their info for the order ~~~ Is when we initialize
-#
-#
-#If no we can let them browse the cupcakes for sale (lol window shopping)
+start_app
